2a9a3da1e485ae4b34465b652ce378c5d6c075db
Danielle Schechter <danielleschechter@Danielles-MacBook-Pro-2.local>
2015-07-23 14:53:30 -0400

    node-sass

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
-var path = require('path');
+
 
 
 var routes = require('./routes/index');
